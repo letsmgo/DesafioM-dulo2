598fa983d2128a63e411a28189cdef6364f4dbc6
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-03 11:39:15 -0300

    Commit apresentação do gerenciador

---
+++ src/br/com/zup/DesafioModulo2.java
@@ -0,0 +1,20 @@
+package br.com.zup;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DesafioModulo2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Map<String, String> funcionarios = new HashMap<>();
+
+        System.out.println("\n             Bem vinde ao gerenciador de funcionários 'YOU CAN'\n");
+        System.out.println("Aqui você consegue inserir novos funcionários, checar as informações de todes,");
+        System.out.println("de uma forma simplificada mas sem perder os atributos tecnológicos!");
+        System.out.println("Vamos começar?!\n");
+
+
+    }
+}
